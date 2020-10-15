d4afdc849b083c0bd431cd5d5e3707ed7c477d9e
Abdellah Chadidi <chadidi97@gmail.com>
2020-10-15 01:12:10 +0100

    Integrate skills

--- components/Sections/Skills.vue
+++ components/Sections/Skills.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="my-auto">
-    <h2 class="mb-5">{{ $t('skills.title') }}</h2>
+    <h2 class="mb-2">{{ $t('skills.title') }}</h2>
 
-    <div class="subheading mb-3">{{ $t('skills.iconsTitle') }}</div>
-    <ul class="list-inline list-icons" style="text-align:center">
-      <li v-for="icon in $t('skills.devIcons')" :key="icon" class="list-inline-item">
-        <i :class="icon"></i>
-      </li>
-    </ul>
-
-    <div class="subheading mb-3">{{ $t('skills.workflow') }}</div>
     <ul class="fa-ul mb-0">
-      <li v-for="item in  $t('skills.itemList')" :key="item">
+      <li v-for="(skill, index) in skills" :key="index">
         <i class="fa-li fa fa-check"></i>
-        {{item}}
+        {{skill.name}} - {{ skill.level }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  data() {
-    return { };
+  computed: {
+    ... mapState({
+      skills: state => state.resume.skills
+    }),
   }
 };
 </script>
